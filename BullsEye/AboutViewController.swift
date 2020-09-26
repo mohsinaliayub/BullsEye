020d77b48e3d9c158037a617310645e69d133a24
Mohsin Ali Ayub <meismosin@gmail.com>
2020-09-27 01:30:55 +0200

    beautiful UI and info scene

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Mohsin Ali Ayub on 26.09.20.
+//  Copyright © 2020 Mohsin Ali Ayub. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
